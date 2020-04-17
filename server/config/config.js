49ea7c8320f48057c07ef58b79188619ef8a011f
Carlos Benavides <praimus666@gmail.com>
2020-04-17 15:33:52 -0500

    Login normal sección 10

--- server/config/config.js
+++ server/config/config.js
@@ -4,6 +4,13 @@ process.env.PORT = process.env.PORT || 3000;
 // Entorno
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
+// Vencimiento del token
+// 60 seg * 60 min * 24 hrs * 30 días
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// Seed de autenticación
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
 // Base de datos
 let urlDB;
 if(process.env.NODE_ENV === 'dev'){
